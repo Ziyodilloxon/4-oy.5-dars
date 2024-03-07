0913c5dc94eb0bde696eb8042b94e04442a6b868
ziyodilloxon <ziyodilloxon8@gmail.com>
2024-03-07 21:17:14 +0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,78 @@
+// 17 misol
+// function ochkolar() {
+//   let x = +prompt("son kiriting");
+//   let y = +prompt("son kiriting");
+//   console.log(x * 2 + y * 3);
+// }
+// ochkolar();
+
+// 18 misol
+// function edabit() {
+//   let a = prompt("So'z kiriting");
+//   let b = "Edabit";
+//   console.log(a + b);
+// }
+// edabit();
+
+// 19m misol
+// function compareSum() {
+//   let num1 = parseInt(prompt("Birinchi sonni kiriting:"));
+//   let num2 = parseInt(prompt("Ikkinchi sonni kiriting:"));
+//   let sum = num1 + num2;
+//   if (sum < 100) {
+//     alert("Rost");
+//   } else {
+//     alert("Yolg'on");
+//   }
+// }
+// compareSum();
+
+// 20 misol
+// function printArray() {
+//   let n = parseInt(prompt("Istalgan sonni kiriting:"));
+//   let arr = [];
+//   for (let i = 1; i <= n; i++) {
+//     arr.push(i);
+//   }
+//   console.log(arr);
+// }
+// printArray();
+
+// 21 misol
+// function oyoqlar() {
+//   let chickens = +prompt("tovuqlar sonini yozing");
+//   let goats = +prompt("qoylar sonini yozing");
+//   let cows = +prompt("sigirlar sonini yozing");
+//   let total = chickens * 2 + goats * 4 + cows * 4;
+
+//   console.log("Jami oyoqlar soni: " + total);
+// }
+// oyoqlar();
+
+// fibonachi sonlar
+// function fibonacci(n) {
+//   if (n <= 1) {
+//     return n;
+//   } else {
+//     return fibonacci(n - 1) + fibonacci(n - 2);
+//   }
+// }
+// const number = parseInt(
+//   prompt("Fibonachi ketma-ketlik uchun bir son kiriting:")
+// );
+// for (let i = 0; i < number; i++) {
+//   console.log(fibonacci(i));
+// }
+
+// // FizzBuzz
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//     console.log("Buzz");
+//   } else {
+//     console.log(i);
+//   }
+// }
